a96e0925c3779be112fddb3c7c76296d5a6e4bec
Josefa <josefacrieza.navarro@smc.pshs.edu.ph>
2026-01-27 19:16:05 +0800

    Navarro_Q3_FA06.py
    A program that takes the user's name, age, and favorite academic subject and formats the three as their student record.

---
+++ Navarro_Q3_FA06.py
@@ -0,0 +1,13 @@
+student = {}
+
+student['name'] = input("What's your name? ")
+student['age'] = input("What's your age? ")
+student['subject'] = input("What's your favorite subject? ")
+
+
+print("Student Record:")
+print()
+
+for key, value in student.items():
+    print(f"{key.capitalize()}: {value}")
+    print()
