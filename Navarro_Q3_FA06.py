bf6de76ee9e7f2207983761342c7d42102e147ff
Josefa <josefacrieza.navarro@smc.pshs.edu.ph>
2026-01-27 19:15:10 +0800

    Delete Navarro_Q3_FA06.py

--- Navarro_Q3_FA06.py
+++
@@ -1,8 +0,0 @@
-student = {}
-
-student['name'] = input("What's your name? ")
-student['age'] = input("What's your age? ")
-student['subject'] = input("What's your favorite subject? ")
-
-for key, value in student.items():
-    print(f"{key.capitalize()}: {value}")
